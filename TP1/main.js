892b8d71d2c0c6faefae93d0f5750044fceaa1dc
Franco Melcon <fmelcon@live.com>
2021-02-11 18:50:05 -0300

    upload tp1 and tp2

---
+++ TP1/main.js
@@ -0,0 +1,14 @@
+//Ejemplo 1
+var nombre = prompt("Ingrese el nombre: ");
+alert("Hola, " + nombre + " como estas?");
+
+//Ejemplo 2
+var numero1 = prompt("Ingrese un numero por favor: ");
+var numero2 = prompt("Ingrese otro numero para sumarle el anterior: ");
+suma = parseInt(numero1) + parseInt(numero2);
+console.log(suma);
+
+//Ejemplo 3
+var texto1 = prompt("Ingrese 1er texto: ");
+var texto2 = prompt("Ingrese 2ndo texto: ");
+alert(texto1 + "  " + texto2);
---
+++ TP2/main.js
@@ -0,0 +1,24 @@
+//Ejercicio 1
+let input = prompt("Ingrese un numero: ");
+input = parseInt(input);
+
+if (input > 1000) {
+    alert("El numero ingresado es mayor a 1000");
+}
+
+//Ejercicio 2
+
+let input2 = prompt("Ingrese un texto:");
+
+if (input2 == "hola") {
+    console.log("Tu entrada es igual a hola")
+}
+
+//Ejercicio 3
+
+let input3 = prompt("Ingrese un valor:")
+input3 = parseInt(input3);
+
+if (input3 > 10 && input3 < 50) {
+    alert("El numero que ingresaste esta entre 10 y el 50!")
+}
